d93b44e2a9ef4f461c8b9775a8bf58a5878ff94c
Manoel Campos <manoelcampos@gmail.com>
2022-12-19 22:56:02 -0300

    Adiciona cálculo da taxa de rendimento da poupança

--- renda-fixa.js
+++ renda-fixa.js
@@ -59,10 +59,34 @@ const trAcumuladaAno = async () => {
 
 }
 
+/**
+ * Converte uma taxa a.m para a.a.
+ * @param {double} taxaMes taxa a.m (em escala 0..100)
+ * @return taxa a.a (em escala 0..100)
+ */
+const taxaMesParaAno = (taxaMes) => {
+    return (Math.pow(1 + taxaMes/100, 12) - 1)*100
+}
+
+/**
+ * Calcula a taxa de rendimento da poupança para o ano atual.
+ * @param {double} selicAno Taxa SELIC a.a (em escala 0..100)
+ * @param {double} trAno Taxa Referencial a.a (em escala 0..100)
+ * @return a taxa da poupança para o ano atual 
+ */
+const rendimentoPoupancaAno = (selicAno, trAno) => {
+    const taxaMes = 0.5
+    const taxaAnual = selicAno > 8.5 ? taxaMesParaAno(taxaMes) : selicAno * 0.7
+    return taxaAnual + trAno
+}
+
 $(async () => {
     const selic = await selicAcumuladaAno()
     $('#selic').val(selic.valor)
 
     const tr = await trAcumuladaAno()
     $('#tr').val(tr.valor)
+
+    const taxaPoupanca = rendimentoPoupancaAno(selic.valor, tr.valor)
+    $('#taxaPoupanca').val(taxaPoupanca)
 })
